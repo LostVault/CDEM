c6822a5224e184f522d045a297aa5c54dfa3a51d
norohind <60548839+norohind@users.noreply.github.com>
2022-04-01 17:01:38 +0300

    Refactor main.py to run as a service, not as a cron-like task

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
+import asyncio
 import dataclasses
 import os
-from DiscordNotifier import DiscordNotifier
+import signal
+import traceback
+from typing import AsyncGenerator
+import aiohttp
+import discord
+import discord.ext.tasks
 import Constants
-import requests
 from CalendarEvent import CalendarEvent
 from CalendarEventDiscordTemplate import CalendarEventDiscordTemplate
 from datetime import datetime
 import DB
 
 
+# As a DPY background task:
 # 1. Request api
 # 2. Check if we have new records
 # 3. Send new records to discord
@@ -18,53 +24,90 @@ def parse_timestamp(timestamp: str) -> datetime:
     return datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%S%z')
 
 
-r = requests.get(Constants.EVENTS_FEED)
-r.raise_for_status()
+async def new_events_generator() -> AsyncGenerator[str, None]:
+    async with aiohttp.ClientSession() as session:
+        async with session.get(Constants.EVENTS_FEED) as r:
+            r.raise_for_status()
+            json_response = await r.json()
 
-raw_events = r.json()['data']
-included = r.json()['included']
+    raw_events = json_response['data']
+    included = json_response['included']
 
-events = list()
+    events = list()
 
-for raw_event in raw_events:
-    event_id = raw_event['id']
-    raw_event_relationships = raw_event['relationships']
-    activity_type_id = raw_event_relationships['field_cal_activity_types']['data'][0]['id']
+    for raw_event in raw_events:
+        event_id = raw_event['id']
+        raw_event_relationships = raw_event['relationships']
+        activity_type_id = raw_event_relationships['field_cal_activity_types']['data'][0]['id']
 
-    activity_name: str | None = None
-    for include in included:
-        if include['id'] == activity_type_id:
-            activity_name = include['attributes']['name']
-            break
+        activity_name: str | None = None
+        for include in included:
+            if include['id'] == activity_type_id:
+                activity_name = include['attributes']['name']
+                break
 
-    raw_event_attributes = raw_event['attributes']
-    events.append(
-        CalendarEvent(
-            drupal_id=event_id,
-            title=raw_event_attributes['title'],
-            start_datetime=parse_timestamp(raw_event_attributes['field_cal_start_date_time']),
-            end_datetime=parse_timestamp(raw_event_attributes['field_cal_end_date_time']),
-            activity_type=activity_name,
-            location=raw_event_attributes['field_cal_location'],
-            description=raw_event_attributes['field_cal_plain_description'],
-            eligibility=raw_event_attributes['field_cal_eligibility'],
-            requirements=raw_event_attributes['field_cal_requirements'],
-            addition_requirements=raw_event_attributes['field_cal_add_reqs'],
-            info_link=raw_event_attributes['field_cal_link']['uri'],
-            video_link=raw_event_attributes['field_cal_video_url'],
-            image_link=raw_event_attributes['field_cal_image_url']
+        raw_event_attributes = raw_event['attributes']
+        events.append(
+            CalendarEvent(
+                drupal_id=event_id,
+                title=raw_event_attributes['title'],
+                start_datetime=parse_timestamp(raw_event_attributes['field_cal_start_date_time']),
+                end_datetime=parse_timestamp(raw_event_attributes['field_cal_end_date_time']),
+                activity_type=activity_name,
+                location=raw_event_attributes['field_cal_location'],
+                description=raw_event_attributes['field_cal_plain_description'],
+                eligibility=raw_event_attributes['field_cal_eligibility'],
+                requirements=raw_event_attributes['field_cal_requirements'],
+                addition_requirements=raw_event_attributes['field_cal_add_reqs'],
+                info_link=raw_event_attributes['field_cal_link']['uri'],
+                video_link=raw_event_attributes['field_cal_video_url'],
+                image_link=raw_event_attributes['field_cal_image_url']
+            )
         )
-    )
 
-discord_notifier = DiscordNotifier(int(os.environ['CDEM_DISCORD_CHANNEL_ID']))
-discord_notifier.run_in_thread(os.environ['CDEM_DISCORD_TOKEN'])
+    # noinspection PyTypeChecker
+    events = sorted(events)
+    for event in events:
+        if DB.CalendarEvents.select().where(DB.CalendarEvents.drupal_id == event.drupal_id).count() == 0:
+            # We don't have this record in DB
+            yield str(CalendarEventDiscordTemplate(event))
+            DB.CalendarEvents.create(**dataclasses.asdict(event))
 
-# noinspection PyTypeChecker
-events = sorted(events)
-for event in events:
-    if DB.CalendarEvents.select().where(DB.CalendarEvents.drupal_id == event.drupal_id).count() == 0:
-        # We don't have this record in DB
-        discord_notifier.send(str(CalendarEventDiscordTemplate(event)))
-        DB.CalendarEvents.create(**dataclasses.asdict(event))
 
-discord_notifier.stop()
+class CommunityDrivenEventsMonitor(discord.Client):
+    def __init__(self, channel_id: int, *args, **kwargs):
+        super().__init__(**kwargs)
+        signal.signal(signal.SIGTERM, self.shutdown)
+        signal.signal(signal.SIGINT, self.shutdown)
+        print('Shutdown callbacks registered')
+        self.channel_id = channel_id
+
+    async def on_ready(self):
+        print(f'Logged in as {self.user} (ID: {self.user.id})')
+        self.notifier_background_task.start()
+
+    def shutdown(self, sig, frame):
+        print(f'Shutting down by signal: {sig}')
+        asyncio.create_task(self.close())
+
+    @discord.ext.tasks.loop(seconds=30.0)
+    async def notifier_background_task(self):
+        # noinspection PyBroadException
+        try:
+            channel: discord.channel.TextChannel = self.get_channel(self.channel_id)
+            async for msg in new_events_generator():
+                message = await channel.send(msg)
+                await message.publish()
+
+        except Exception:
+            print(traceback.format_exc())
+
+    @notifier_background_task.before_loop
+    async def notifier_wait_ready(self):
+        await self.wait_until_ready()
+
+
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
+client = CommunityDrivenEventsMonitor(int(os.environ['CDEM_DISCORD_CHANNEL_ID']), loop=loop)
+loop.run_until_complete(client.start(os.environ['CDEM_DISCORD_TOKEN']))
